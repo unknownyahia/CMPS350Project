ec7a1187bfdf1311f92976b08641dafe6c56023b
unknownyahia <zagadyahia2002@gmail.com>
2025-04-14 13:53:46 +0300

    remove the old ones

--- Phase1/WebAPI/app/api/classes/route.js
+++
@@ -1,7 +0,0 @@
-import createResponse from "../../response.js";
-import classes from "../../data/classes/classes.js";
-export async function POST(request) {
-  const data = await request.json();
-  classes.createClass(data);
-  return createResponse({ data: "done" });
-}
--- Phase1/WebAPI/app/api/instructors/route.js
+++
@@ -1,6 +0,0 @@
-import createResponse from "../../response.js";
-import instructors from "../../data/instructors/instructors.js";
-
-export async function GET(request) {
-  return createResponse({ data: await instructors.getInstructorsData() });
-}
